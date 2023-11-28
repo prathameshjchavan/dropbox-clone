cbe0367df75cc48eb2c7f035e592c38a90c3a3bd
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-28 15:38:18 +0530

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<main className="">
 			<h1>Dropbox Clone</h1>
+
+			<Button>Here is a demo button</Button>
 		</main>
 	);
 }
