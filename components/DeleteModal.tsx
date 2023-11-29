d71cfd67d048cfb8251e682dba3781d8b3bce01a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-29 22:35:04 +0530

    implemented rename file functionality

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -74,6 +74,7 @@ export function DeleteModal() {
 					<Button
 						type="submit"
 						size="sm"
+						variant="destructive"
 						className="px-3 flex-1"
 						onClick={() => deleteFile()}
 					>
---
+++ components/RenameModal.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import useAppStore from "@/store/store";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+
+const RenameModal = () => {
+	const { user } = useUser();
+	const [input, setInput] = useState("");
+
+	const [isRenameModalOpen, setIsRenameModalOpen, filename, fileId] =
+		useAppStore((state) => [
+			state.isRenameModalOpen,
+			state.setIsRenameModalOpen,
+			state.filename,
+			state.fileId,
+		]);
+
+	const renameFile = async () => {
+		if (!user || !fileId) return;
+
+		try {
+			await updateDoc(doc(db, "users", user.id, "files", fileId), {
+				fileName: input,
+			});
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setInput("");
+			setIsRenameModalOpen(false);
+		}
+	};
+
+	return (
+		<Dialog
+			open={isRenameModalOpen}
+			onOpenChange={(isOpen) => {
+				setIsRenameModalOpen(isOpen);
+			}}
+		>
+			<DialogContent className="sm:max-w-md">
+				<DialogHeader>
+					<DialogTitle>Rename the file</DialogTitle>
+				</DialogHeader>
+
+				<Input
+					id="link"
+					defaultValue={filename}
+					onChange={(e) => setInput(e.target.value)}
+					onKeyDownCapture={(e) => {
+						if (e.key === "Enter") {
+							renameFile();
+						}
+					}}
+				/>
+
+				<div className="flex space-x-2 py-3">
+					<Button
+						size="sm"
+						className="px-3 flex-1"
+						variant="ghost"
+						onClick={() => setIsRenameModalOpen(false)}
+					>
+						<span className="sr-only">Cancel</span>
+						<span>Cancel</span>
+					</Button>
+
+					<Button
+						type="submit"
+						size="sm"
+						className="px-3 flex-1"
+						onClick={() => renameFile()}
+					>
+						<span className="sr-only">Delete</span>
+						<span>Delete</span>
+					</Button>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default RenameModal;
