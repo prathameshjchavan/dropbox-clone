23c7c60dd955e921838357b7fcb2cffadf4c11fb
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-29 13:05:42 +0530

    added onDrop function

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -1,18 +1,42 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
 import DropzoneComponent from "react-dropzone";
 
 const Dropzone = () => {
+	const [loading, setLoading] = useState(false);
+	const { user, isSignedIn, isLoaded } = useUser();
+
 	// max file size 2MB
 	const maxSize = 20971520;
 
+	const uploadPost = async (selectedFile: File) => {
+		if (loading || !user) return;
+
+		setLoading(true);
+
+		// do what needs to be done...
+
+		setLoading(false);
+	};
+
+	const onDrop = (acceptedFiles: File[]) => {
+		acceptedFiles.forEach((file) => {
+			const reader = new FileReader();
+
+			reader.onabort = () => console.log("file reading was aborted");
+			reader.onerror = () => console.log("file reading has failed");
+			reader.onload = async () => {
+				await uploadPost(file);
+			};
+			reader.readAsArrayBuffer(file);
+		});
+	};
+
 	return (
-		<DropzoneComponent
-			minSize={0}
-			maxSize={maxSize}
-			onDrop={(acceptedFiles) => console.log(acceptedFiles)}
-		>
+		<DropzoneComponent minSize={0} maxSize={maxSize} onDrop={onDrop}>
 			{({
 				getRootProps,
 				getInputProps,
