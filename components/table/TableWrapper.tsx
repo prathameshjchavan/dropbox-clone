844d0229cde100533608d0f1ac99d06195e26dee
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-29 19:24:23 +0530

    added skeleton loader

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase";
+import { Skeleton } from "../ui/skeleton";
 
 interface TableWrapperProps {
 	skeletonFiles: FileType[];
@@ -48,9 +49,40 @@ const TableWrapper = ({ skeletonFiles }: TableWrapperProps) => {
 		setInitialFiles(files);
 	}, [docs]);
 
+	if (docs?.docs === undefined)
+		return (
+			<div className="flex flex-col">
+				<Button variant="outline" className="ml-auto w-36 h-10 mb-5">
+					<Skeleton className="h-5 w-full" />
+				</Button>
+
+				<div className="border rounded-lg">
+					<div className="border-b h-12" />
+					{skeletonFiles.map((file) => (
+						<div
+							key={file.id}
+							className="flex items-center space-x-4 p-5 w-full"
+						>
+							<Skeleton className="h-12 w-12" />
+							<Skeleton className="h-12 w-full" />
+						</div>
+					))}
+
+					{skeletonFiles.length == 0 && (
+						<div className="flex items-center space-x-4 p-5 w-full">
+							<Skeleton className="h-12 w-12" />
+							<Skeleton className="h-12 w-full" />
+						</div>
+					)}
+				</div>
+			</div>
+		);
+
 	return (
-		<div>
+		<div className="flex flex-col space-y-5 pb-10">
 			<Button
+				variant="outline"
+				className="ml-auto w-fit"
 				onClick={() => setSort((sort) => (sort === "desc" ? "asc" : "desc"))}
 			>
 				Sort By {sort === "desc" ? "Oldest" : "Newest"}
